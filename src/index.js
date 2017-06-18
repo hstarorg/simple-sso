bf2f5670c97c158463a2cfd9316384a357e0bdd2
Hstarorg <hm910705@163.com>
2017-06-18 17:22:02 +0800

    Change login page to use layui

--- src/index.js
+++ src/index.js
@@ -8,9 +8,14 @@ const config = require('./config');
 // const server = http.createServer(app);
 const options = {
   key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'cert','certificate.pem'))
+  cert: fs.readFileSync(path.join(__dirname, 'cert', 'certificate.pem'))
 };
-const server = spdy.createServer(options, app);
+let server;
+if (config.useHttp2) {
+  server = spdy.createServer(options, app);
+} else {
+  server = http.createServer(app);
+}
 
 process.on('uncaughtException', err => {
   console.error('uncaughtException:', err);
