f7d2ef11e0b539b3bf6de3b9f136589d366cbf19
Hstarorg <hm910705@163.com>
2017-08-03 22:53:12 +0800

    Fixed username length judge

--- src/assets/js/register.js
+++ src/assets/js/register.js
@@ -17,7 +17,7 @@ new Vue({
       if (!this.user.UserName || !this.user.Password) {
         return AppTools.messageBox.toast('请输入账户密码');
       }
-      if (!this.user.UserName.length < 4) {
+      if (this.user.UserName.length < 4) {
         return AppTools.messageBox.toast('账户名至少4位');
       }
       if (this.user.Password !== this.user.Password2) {
