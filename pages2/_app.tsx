a90f7e36da87d3508afff0d77958d822fd4e2df2
hstarorg <hm910705@163.com>
2023-06-12 23:30:38 +0800

    chore: adjust folder, delete unused files

--- pages2/_app.tsx
+++
@@ -1,9 +0,0 @@
-import "../styles/globals.css";
-import "../node_modules/antd/dist/antd.min.css";
-import type { AppProps } from "next/app";
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
--- pages2/_middleware.ts
+++
@@ -1,5 +0,0 @@
-import type { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
-
-export function middleware(req: NextRequest) {
-
-}
